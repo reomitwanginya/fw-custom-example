c966bbaf8eaf899fbd77ed39b07ac9e70ce812e4
GitHub git update Action <action@github.com>
2025-12-01 01:47:12 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_f407-discovery.h
+++ generated/controllers/generated/signature_f407-discovery.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 2444095967
-#define TS_SIGNATURE "rusEFI main.2025.11.30.f407-discovery.2444095967"
+#define TS_SIGNATURE "rusEFI main.2025.12.01.f407-discovery.2444095967"
